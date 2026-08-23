379a3067f41d3e63a1e177101d9fda02384796f7
Stuti Agarwal <stutiagarwal52@gmail.com>
2026-08-23 14:25:58 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Palindrome {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a string");
+        String s = sc.nextLine();
+        String rev = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev = rev + s.charAt(i);
+        }
+        if (s.equals(rev)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a palindrome");
+        }
+    }
+}
+
+
+
+                    
