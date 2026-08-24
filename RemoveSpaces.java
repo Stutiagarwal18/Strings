f51b12041b66d5b4e03fa22a42ce17527831d133
Stuti Agarwal <stutiagarwal52@gmail.com>
2026-08-24 22:37:11 +0530

    Add files via upload

---
+++ RemoveSpaces.java
@@ -0,0 +1,17 @@
+//wap to remove all spaces from string
+import java.util.*;
+
+class RemoveSpaces {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String result = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                result = result + s.charAt(i);
+            }
+        }
+        System.out.println(result);
+
+    }
+}
